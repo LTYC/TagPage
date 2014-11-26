437babfb7a320b5fcd7150e29fb5af5695db5d85
Freeeed <friedrich@Friedrichs-MacBook-Pro.local>
2014-11-26 15:36:52 +0100

    Added new gulp tasks
    run „gulp admin_default“ to run all admin tasks

--- gulpfile.js
+++ gulpfile.js
@@ -41,8 +41,32 @@ gulp.task('js', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/less/*.less', ['less']);
-    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch(SRC_PATH + 'less/*.less', ['less']);
+    gulp.watch(SRC_PATH + 'js/**/*.js', ['js']);
 })
 
+gulp.task('admin_less', function () {
+    return gulp.src(SRC_PATH + 'admin/less/main.less')
+        .pipe(less({
+            paths: [path.join('vendor', 'bower_components', 'bootstrap', 'less')]
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(BASE_PATH + 'admin/css'))
+        .pipe(notify({"message": "LESS compiled!"}));
+});
+
+gulp.task('admin_js', function () {
+    return gulp.src(third_party.concat([SRC_PATH + 'admin/js/**/*.js']))
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(BASE_PATH + 'admin/js'))
+        .pipe(notify({"message": "JS compiled!"}));
+});
+
+gulp.task('admin_watch', function () {
+    gulp.watch(SRC_PATH + 'admin/less/*.less', ['less']);
+    gulp.watch(SRC_PATH + 'admin/js/**/*.js', ['js']);
+})
+
+gulp.task('admin_default', ['admin_less', 'admin_js', 'admin_watch']);
 gulp.task('default', ['less', 'js', 'watch']);
